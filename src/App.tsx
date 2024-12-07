ead969b30e74584fe3304668b0846428063605c4
Nilton Caio <ciaonilton@hotmail.com>
2024-12-07 11:52:21 -0300

    criando opções de temas para o site e aplicando efeito

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { ThemeProvider } from 'styled-components'
+
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import Projetos from './containers/Projetos'
 import EstiloGlobal, { Container } from './styled'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
+import { useState } from 'react'
 
 function App() {
+  const [estausandoTemaDark, setestausandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setestausandoTemaDark(!estausandoTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estausandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
